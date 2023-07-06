5c19a31f125305111e7760c3fbefa9b023393a24
yomomma <jezzy.maxwell@gmail.com>
2023-07-06 05:20:19 -0400

    Inventory Mode was still acting irregularly. Refactored again and made a new solution for tracking unique items. Which seems to be working after extensive testing
    Added a Copyright Disclaimer to the HighlightStackablesConfig.java since most of the code was just copy and pasted straight from the Ground Items Plugin GroundItemsConfig.java

--- src/main/java/com/HighlightStackables/HighlightStackablesPlugin.java
+++ src/main/java/com/HighlightStackables/HighlightStackablesPlugin.java
@@ -1,4 +1,5 @@
 package com.HighlightStackables;
+
 import com.google.inject.Provides;
 
 import javax.inject.Inject;
@@ -9,7 +10,6 @@ import net.runelite.api.events.ClientTick;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.ItemDespawned;
 import net.runelite.api.events.ItemSpawned;
-
 
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
@@ -22,93 +22,83 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 import net.runelite.client.plugins.grounditems.GroundItemsConfig;
 import net.runelite.client.plugins.grounditems.GroundItemsPlugin;
-
-
+import net.runelite.client.plugins.grounditems.GroundItemsOverlay;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-
 @Slf4j
 @PluginDescriptor(
 		name = "Highlight Stackables"
 )
 @PluginDependency(GroundItemsPlugin.class)
-public class HighlightStackablesPlugin extends Plugin
-{
+public class HighlightStackablesPlugin extends Plugin {
 	private List<String> spawnedItems;
-
 	private List<String> spawnedItemsUnique;
 
 	@Inject
 	private HighlightStackablesConfig config;
 
 	@Provides
-	HighlightStackablesConfig provideConfig(ConfigManager configManager)
-	{
+	HighlightStackablesConfig provideConfig(ConfigManager configManager) {
 		return configManager.getConfig(HighlightStackablesConfig.class);
 	}
 
 	@Inject
+	private GroundItemsPlugin groundItemsPlugin;
+
+	@Inject
 	private GroundItemsConfig groundItemsConfig;
+
+	@Inject
+	private GroundItemsOverlay groundItemsOverlay;
+
 	@Inject
 	private Client client;
 
 	@Inject
 	private ItemManager itemManager;
 
-
-
-
-
-
 	@Override
-	protected void startUp()
-	{
+	protected void startUp() {
 		spawnedItems = new ArrayList<>();
 		spawnedItemsUnique = new ArrayList<>();
-		config.setOriginalItem(groundItemsConfig.getHighlightItems());
 
+		// Store the original highlighted items from GroundItemsConfig
+		config.setOriginalItem(groundItemsConfig.getHighlightItems());
 	}
 
 	@Override
-	protected void shutDown()
-	{
-
-	groundItemsConfig.setHighlightedItem(config.getOrginalItems());
+	protected void shutDown() {
+		// Restore the original highlighted items to GroundItemsConfig
+		groundItemsConfig.setHighlightedItem(config.getOrginalItems());
 	}
 
 	@Subscribe
-	public void onGameStateChanged(final GameStateChanged event)
-	{
-		if (event.getGameState() == GameState.LOADING)
-		{
+	public void onGameStateChanged(final GameStateChanged event) {
+		if (event.getGameState() == GameState.LOADING) {
 			spawnedItems.clear();
-
-		}
-	}
-	@Subscribe
-	public void onClientTick(ClientTick event)
-	{
-
-		if(spawnedItems.isEmpty() && groundItemsConfig.getHighlightItems() != config.getOrginalItems()){
-			groundItemsConfig.setHighlightedItem(config.getOrginalItems());
-
 		}
 	}
 
 	@Subscribe
-	public void onConfigChanged(ConfigChanged event)
-	{
-		if (event.getGroup().equals("highlightStackables"))
-		{
+	public void onClientTick(ClientTick event) {
+		// Check if spawnedItems is empty and if there are changes in original highlighted items
+		if (spawnedItems.isEmpty() && !groundItemsConfig.getHighlightItems().equals(config.getOrginalItems())) {
+			// Restore the original highlighted items
 			groundItemsConfig.setHighlightedItem(config.getOrginalItems());
-
 		}
 	}
 
+	@Subscribe
+	public void onConfigChanged(ConfigChanged event) {
+		if (event.getGroup().equals("highlightStackables")) {
+			// Update the highlighted items in GroundItemsConfig
+			groundItemsConfig.setHighlightedItem(config.getOrginalItems());
+		}
+	}
 
 	@Subscribe
 	public void onItemSpawned(ItemSpawned itemSpawned) {
@@ -118,80 +108,58 @@ public class HighlightStackablesPlugin extends Plugin
 		final ItemComposition itemComposition = itemManager.getItemComposition(id);
 		String exclusionList = groundItemsConfig.getHiddenItems().toString();
 
-
-		if (itemComposition.isStackable()&& !exclusionList.contains(itemComposition.getName()) ) {
-
-
-			String oldList = groundItemsConfig.getHighlightItems().toString();
-
-
-
-
-			if(config.inventoryStackable()) {
-				if (!oldList.contains(itemComposition.getName()) && inventory.contains(itemComposition.getId())) {
+		if (itemComposition.isStackable() && !exclusionList.contains(itemComposition.getName())) {
+			if (config.inventoryStackable()) {
+				if (inventory.contains(itemComposition.getId())) {
 					spawnedItems.add(itemComposition.getName());
 					groundItemsConfig.setHighlightedItem(config.getOrginalItems());
-
 					HashSet<String> uniqueSet = new HashSet<>();
-
-					// Create a new ArrayList to store unique elements
-					ArrayList<String> spawnedItemsUnique = new ArrayList<>();
-
-					for (String element : spawnedItems) {
-						// Add the element to the uniqueSet if it doesn't exist already
-						if (uniqueSet.add(element)) {
-							// If the element is successfully added to the uniqueSet,
-							// add it to the spawnedItemsUnique list as well
-							spawnedItemsUnique.add(element);
-						}
-					}
-
-
-
-
-					String formatedString = spawnedItemsUnique.toString();
-					formatedString = formatedString.substring(1, formatedString.length() - 1);
-
-					groundItemsConfig.setHighlightedItem(config.getOrginalItems() + "," + formatedString);
-
-				}
-			}else
-			{
-				if (!oldList.contains(itemComposition.getName()) && !exclusionList.contains(itemComposition.getName())){
-					spawnedItems.add(itemComposition.getName());
-					groundItemsConfig.setHighlightedItem(config.getOrginalItems());
-
-
-
-					HashSet<String> uniqueSet = new HashSet<>();
-
 
 					ArrayList<String> spawnedItemsUnique = new ArrayList<>();
 
 					for (String element : spawnedItems) {
-
-						if (uniqueSet.add(element)) {
-
+						boolean alreadyExists = false;
+						for (String uniqueElement : spawnedItemsUnique) {
+							if (element.contains(uniqueElement)) {
+								alreadyExists = true;
+								break;
+							}
+						}
+						if (!alreadyExists) {
 							spawnedItemsUnique.add(element);
 						}
 					}
 
-
-					String formatedString = spawnedItemsUnique.toString();
-					formatedString = formatedString.substring(1, formatedString.length() - 1);
-
-					groundItemsConfig.setHighlightedItem(config.getOrginalItems() + "," + formatedString);
-
+					String formattedString = spawnedItemsUnique.toString();
+					formattedString = formattedString.substring(1, formattedString.length() - 1);
+					groundItemsConfig.setHighlightedItem(config.getOrginalItems() + "," + formattedString);
 				}
+			} else {
+				spawnedItems.add(itemComposition.getName());
+				groundItemsConfig.setHighlightedItem(config.getOrginalItems());
+				HashSet<String> uniqueSet = new HashSet<>();
+				for (String element : spawnedItems) {
+					boolean alreadyExists = false;
+					for (String uniqueElement : spawnedItemsUnique) {
+						if (element.contains(uniqueElement)) {
+							alreadyExists = true;
+							break;
+						}
+					}
+					if (!alreadyExists) {
+						spawnedItemsUnique.add(element);
+					}
+				}
+
+				String formattedString = spawnedItemsUnique.toString();
+				formattedString = formattedString.substring(1, formattedString.length() - 1);
+				groundItemsConfig.setHighlightedItem(config.getOrginalItems() + "," + formattedString);
 			}
-
-
-
 		}
 	}
 
 	@Subscribe
-	public void onItemDespawned(ItemDespawned itemDespawned){
+	public void onItemDespawned(ItemDespawned itemDespawned) {
 		final ItemContainer inventory = client.getItemContainer(InventoryID.INVENTORY);
 		final TileItem item = itemDespawned.getItem();
 		final int id = item.getId();
@@ -200,20 +168,15 @@ public class HighlightStackablesPlugin extends Plugin
 		spawnedItems.remove(itemComposition.getName());
 
 		HashSet<String> uniqueSet = new HashSet<>();
-
-		// Create a new ArrayList to store unique elements
 		ArrayList<String> spawnedItemsUnique = new ArrayList<>();
 
 		for (String element : spawnedItems) {
-
 			if (uniqueSet.add(element)) {
-
 				spawnedItemsUnique.add(element);
 			}
 		}
-		String formatedString = spawnedItemsUnique.toString();
-		formatedString = formatedString.substring(1, formatedString.length() - 1);
-		groundItemsConfig.setHighlightedItem(config.getOrginalItems() + "," + formatedString);
-
+		String formattedString = spawnedItemsUnique.toString();
+		formattedString = formattedString.substring(1, formattedString.length() - 1);
+		groundItemsConfig.setHighlightedItem(config.getOrginalItems() + "," + formattedString);
 	}
 }
